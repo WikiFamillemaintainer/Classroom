19f7661887aabec9d3de24337b3a62ae8c443713
WikiFamillemaintainer <122366189+WikiFamillemaintainer@users.noreply.github.com>
2023-02-07 13:27:20 +0100

    cool

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ function username (){
 let user_name=prompt("enter user_name name")
 let password=prompt("enter password")
 if (user_name=="urenzetti"&&password==2352323){
-    window.location.href='https://wikifamillemaintainer.github.io/Classroom/urenzetti.html'
+    window.location.href='urenzetti.html'
 }
 
 }
